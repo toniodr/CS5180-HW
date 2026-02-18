7d2f6fcf16011cc05bd324d7aefbb230a6135485
Antonio Duran <aduran1030@gmail.com>
2026-02-17 23:18:39 -0800

    done

--- hw1/search_engine.py
+++ hw1/search_engine.py
@@ -1,9 +1,9 @@
 #-------------------------------------------------------------
-# AUTHOR: Antonio Duran
+# AUTHOR: Antonio Duran 
 # FILENAME: search_engine.py
-# SPECIFICATION: 
+# SPECIFICATION: description of the program
 # FOR: CS 5180- Assignment #1
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 1 hour
 #-----------------------------------------------------------*/
 
 # ---------------------------------------------------------
@@ -29,23 +29,38 @@ with open('collection.csv', 'r') as csvfile:
 # ---------------------------------------------------------
 # --> add your Python code here
 
-print(?)
+print(documents)
 
 # ---------------------------------------------------------
 # Instantiate CountVectorizer informing 'word' as the analyzer, Porter stemmer as the tokenizer, stop_words as the identified stop words,
 # unigrams and bigrams as the ngram_range, and binary representation as the weighting scheme
 # ---------------------------------------------------------
 # --> add your Python code here
+from nltk import word_tokenize
+import nltk 
+nltk.download('punkt')
 
-vectorizer = ?
+class StemTokenizer:
+    def __init__(self):
+        self.stemmer = PorterStemmer()
+    def __call__(self, doc):
+        return [self.stemmer.stem(t) for t in word_tokenize(doc)]
+
+vectorizer = CountVectorizer( 
+    analyzer    = 'word',
+    tokenizer   = StemTokenizer(),
+    stop_words  = 'english',
+    ngram_range = (1, 2),
+    binary      = True 
+)
 
 # ---------------------------------------------------------
 # Fit the vectorizer to the documents and encode the them
 # ---------------------------------------------------------
 # --> add your Python code here
 
-vectorizer.fit(?)
-document_matrix = vectorizer.transform(?)
+vectorizer.fit(documents)
+document_matrix = vectorizer.transform(documents)
 
 # ---------------------------------------------------------
 # Inspect vocabulary
@@ -57,16 +72,16 @@ print("Vocabulary:", vectorizer.get_feature_names_out().tolist())
 # ---------------------------------------------------------
 # --> add your Python code here
 
-query = ?
-query_vector = vectorizer.transform(?)
+query = ["I love dogs"]
+query_vector = vectorizer.transform(query)
 
 # ---------------------------------------------------------
 # Convert matrices to plain Python lists
 # ---------------------------------------------------------
 # --> add your Python code here
 
-doc_vectors = ?
-query_vector = ?
+doc_vectors = document_matrix.toarray()
+query_vector = query_vector.toarray()[0]
 
 # ---------------------------------------------------------
 # Compute dot product
@@ -75,9 +90,16 @@ query_vector = ?
 scores = []
 # --> add your Python code here
 
+for doc_vector in doc_vectors:
+    score = sum(doc_vector[i] * query_vector[i] for i in range (len(query_vector)))
+    scores.append(score)
+
 # ---------------------------------------------------------
 # Sort documents by score (descending)
 # ---------------------------------------------------------
 
 ranking = []
 # --> add your Python code here
+for i, score in enumerate(scores):
+    ranking.append((i, score))
+ranking.sort(key=lambda x: x[1], reverse=True)
